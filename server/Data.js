53bdad1e449add1db06ae68b5631f22a574c567c
JT Yim <JT@JTYim.com>
2016-02-02 19:03:09 -0500

    Get req contd

---
+++ server/Data.js
@@ -0,0 +1,27 @@
+var respData = [];
+
+exports.responseData = respData;
+
+
+
+var respData =  [ {
+      createdAt: 'time',
+      username: 'user',
+      text: 'message',
+      roomname: 'room'
+    },
+    {
+      createdAt: 'time',
+      username: 'user',
+      text: 'message',
+      roomname: 'room'
+    },
+    {
+      createdAt: 'time',
+      username: 'user',
+      text: 'message',
+      roomname: 'room'
+    } 
+]
+
+
